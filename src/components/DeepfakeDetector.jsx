41890ee585d280a4412e6c70229a6bd6620cedb3
Gowri-Arun <208835626+Gowri-Arun@users.noreply.github.com>
2025-07-18 01:55:40 +0530

    Railway deployment

--- src/components/DeepfakeDetector.jsx
+++ src/components/DeepfakeDetector.jsx
@@ -26,7 +26,7 @@ const DeepfakeDetector = () => {
       const formData = new FormData();
       formData.append("file", uploadedImage);
 
-      const response = await fetch("http://localhost:8000/analyze", {
+      const response = await fetch("https://VantaAI-DFD.up.railway.app/analyze", {
         method: "POST",
         body: formData,
       });
